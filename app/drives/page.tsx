bcc4ae511b4ea7c98b6883b74a6b5bf67045abe5
RishiReddy9 <rishicode9@gmail.com>
2025-10-04 23:19:43 +0530

    Handling route issues in deployment

--- app/drives/page.tsx
+++ app/drives/page.tsx
@@ -4,6 +4,7 @@ import useSWR from "swr";
 import Link from "next/link";
 import { useState } from "react";
 import { Input } from "@heroui/input";
+import AuthGuard from "@/components/AuthGuard";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
@@ -19,6 +20,7 @@ export default function DrivesPage() {
   });
 
   return (
+    <AuthGuard>
     <section className="p-6">
       <h1 className="text-3xl font-bold mb-6 text-center">🎯 Drives & Experiences</h1>
 
@@ -66,5 +68,6 @@ export default function DrivesPage() {
         )}
       </div>
     </section>
+    </AuthGuard>
   );
 }
