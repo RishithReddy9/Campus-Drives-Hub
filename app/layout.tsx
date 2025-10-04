3bf653cdae9e70f40ddd86ad28223900fd22db89
RishiReddy9 <rishicode9@gmail.com>
2025-10-04 17:56:16 +0530

    Fixed Default Theme issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
       <body>
         <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
